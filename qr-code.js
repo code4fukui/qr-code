2443603a195d34b1b1712554a987b1df8c08e715
Taisuke Fukuno <fukuno@jig.jp>
2021-07-26 22:31:47 +0900

    support realtime sync

--- qr-code.js
+++ qr-code.js
@@ -3,6 +3,9 @@ import { QR } from "https://taisukef.github.io/qrcode-generator/es/QR.js";
 class QRCode extends HTMLElement {
   constructor (param) {
     super();
+    this.set(param);
+  }
+  set(param) {
     const val = this.textContent.trim() || param || document.location.href;
     this.textContent = "";
     const errCorrectionLevel = 0; //  0: L, 1: M, 2: Q, 3: H
@@ -21,31 +24,39 @@ class QRCode extends HTMLElement {
     canvas.width = canvas.height = qw;
     const g = canvas.getContext("2d");
   
-    const draw = () => {
-      const idata = new Uint8ClampedArray(qw * qw * 4);
-      for (let i = 0; i < idata.length / 4; i++) {
-        idata[i * 4 + 0] = 255;
-        idata[i * 4 + 1] = 255;
-        idata[i * 4 + 2] = 255;
-        idata[i * 4 + 3] = 255;
-      }
-      for (let i = 0; i < iw; i++) {
-        for (let j = 0; j < iw; j++) {
-          const c = data[i][j] ? 0 : 255;
-          for (let k = 0; k < r * r; k++) {
-            const x = (i + w) * r + Math.floor(k / r);
-            const y = (j + w) * r + (k % r);
-            idata[(x + y * qw) * 4] = c;
-            idata[(x + y * qw) * 4 + 1] = c;
-            idata[(x + y * qw) * 4 + 2] = c;
-            idata[(x + y * qw) * 4 + 3] = 255;
-          }
+    const idata = new Uint8ClampedArray(qw * qw * 4);
+    for (let i = 0; i < idata.length / 4; i++) {
+      idata[i * 4 + 0] = 255;
+      idata[i * 4 + 1] = 255;
+      idata[i * 4 + 2] = 255;
+      idata[i * 4 + 3] = 255;
+    }
+    for (let i = 0; i < iw; i++) {
+      for (let j = 0; j < iw; j++) {
+        const c = data[i][j] ? 0 : 255;
+        for (let k = 0; k < r * r; k++) {
+          const x = (i + w) * r + Math.floor(k / r);
+          const y = (j + w) * r + (k % r);
+          idata[(x + y * qw) * 4] = c;
+          idata[(x + y * qw) * 4 + 1] = c;
+          idata[(x + y * qw) * 4 + 2] = c;
+          idata[(x + y * qw) * 4 + 3] = 255;
         }
       }
-      const imgdata = new ImageData(idata, qw, qw);
-      g.putImageData(imgdata, 0, 0);
-    };
-    draw();
+    }
+    const imgdata = new ImageData(idata, qw, qw);
+    g.putImageData(imgdata, 0, 0);
+    
+    // check change
+    const observer = new MutationObserver(() =>  {
+      observer.disconnect();
+      this.set();
+    });
+    observer.observe(this, { subtree: true, childList: true });
+  }
+  set value(param) {
+    //console.log("param", param)
+    this.set(param);
   }
 }
 
